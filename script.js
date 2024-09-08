6d1663f1dc941d18f8482219cdcaad6f808a922f
Dmitriy Yaryshev <krestovcode@gmail.com>
2024-09-08 15:43:21 +0300

    add const and functions

---
+++ script.js
@@ -0,0 +1,44 @@
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+const taskForm = document.getElementById('addTask');
+const taskList = document.getElementById('list');
+const taskCount = document.getElementById('taskCount');
+
+function saveTask() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask() {
+    taskList.innerHTML = '';
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <button onclick="deleteTask(${index})"><input class="radio" type="radio" id="radio-input"></button>
+        <p>${task.task}</p>
+    `;
+    taskList.appendChild(li);
+    });
+    taskCount.textContent = tasks.length;
+}
+
+
+function addTask(e) {
+    e.preventDefault();
+    const newTask = {
+        task: document.getElementById('task').value
+    };
+    tasks.push(newTask);
+    saveTask();
+    renderTask();
+    taskForm.reset();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    saveTask();
+    renderTask();
+}
+
+taskForm.addEventListener('submit', addTask);
+
+renderTask();
